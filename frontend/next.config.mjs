1a95e8195128d74367999492e921a3f9184580db
VikashAlex <vkumar637525@gmail.com>
2025-12-09 12:00:22 +0530

    nextConfig file

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,10 +3,15 @@ const nextConfig = {
     images: {
         remotePatterns: [
             {
+                protocol: 'https',
+                hostname: 'fullstack-portfolio-jran.onrender.com',
+                pathname: '/**',
+            },
+            {
                 protocol: 'http',
                 hostname: 'localhost',
                 port: '8000',
-                pathname: '/**', // ya specific path like '/uploads/**'
+                pathname: '/**',
             },
         ],
     },
